5e1622b9fd64cdd573ed9a0a6e7f9a3c03638d3d
sofiavetoshkina <sofia@vetoshkin.ru>
2024-11-05 02:42:14 +0300

    add in main.cpp

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -18,5 +18,16 @@ int main() {
     array.pushBack(new Triangle<int>({Vertex<int>(0, 0), Vertex<int>(3, 0), Vertex<int>(0, 3)}));
     std::cout << "Total area: " << array.totalArea() << std::endl;
     
+    FigureArray<Square<int>> array1;
+    
+    array1.pushBack(Square<int>(Vertex<int>(0, 0), 5));
+    array1.pushBack(Square<int>(Vertex<int>(1, 1), 5));
+    double area = 0.0;
+    for (size_t i = 0; i < array1.size(); ++i) {
+        std::cout << "Square " << i << ": " << array1[i] << std::endl;
+        area += double(array1[i]);
+    }
+    std::cout << "Total area array of squares: " << area << std::endl;
+
     return 0;
 }
